b2d8b05b56907a3e05f0e01ce6a2d62e398a7f2e
DESKTOP-UFN9P75\paulo <paulo.flesch@gmail.com>
2021-12-02 13:22:55 -0300

    Desafio

---
+++ js/tagueamento.js
@@ -0,0 +1,38 @@
+// Preencha este arquivo com qualquer código que você necessite para realizar a
+// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
+// Manager. No último caso, não é necessário implementar a tag <noscript>.
+// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
+// para fazer a sua coleta.
+// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+    ga('create', 'UA-12345-6', 'auto');
+    ga('send', 'pageview', location.pathname);
+
+
+$(document).ready(function() {
+
+    $('.menu-lista-contato').click(function(){
+        ga('send', 'event', 'menu', 'entre_em_contato', 'link_externo');
+    });
+
+    $('.menu-lista-download').click(function(){
+        ga('send', 'event', 'menu', 'download_pdf', 'download_pdf');
+    });
+
+    $('.card-montadoras').click(function(){
+        ga('send', 'event', 'analise', 'ver_mais', $(this).attr("data-id"));
+    });
+
+    $('input').change(function() {
+        ga('send', 'event', 'contato', $(this).attr("id"), "preencheu");
+    });
+
+    $('form').bind('ajax:complete', function() {
+        console.log('teste');
+    });
+
+});
